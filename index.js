c0849184d99c5dcdd99d0b85e1206ab83715d030
TrungHieu <hieuhi123@gmail.com>
2023-05-30 20:27:06 +0700

    new

---
+++ index.js
@@ -0,0 +1,24 @@
+{
+    "items": [
+    {
+    "name": "ASP.NET MVC",
+    "category": "asp.netMVC",
+    "image": "1.png"
+    },
+    {
+    "name": "Node.js",
+    "category": "nodeJS",
+    "image": "2.png"
+    },
+    {
+    "name": "React.js",
+    "category": "reactjs",
+    "image": "3.png"
+    },
+    {
+    "name": "TypeScript",
+    "category": "typescript",
+    "image": "4.png"
+    }
+    ]
+    }
